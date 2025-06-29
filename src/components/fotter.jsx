813d046aad9a6821244592a69a59f43a5c3b6794
Mohammed Alsahli <102217045+MohammedSaudAlsahli@users.noreply.github.com>
2025-06-30 00:10:34 +0300

    -

---
+++ src/components/fotter.jsx
@@ -0,0 +1,15 @@
+function Fotter() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <a href="https://github.com/MohammedSaudAlsahli" target="_blank">
+        Github
+      </a>
+
+      <p>all right reversed {currentYear} </p>
+    </>
+  );
+}
+
+export default Fotter;
