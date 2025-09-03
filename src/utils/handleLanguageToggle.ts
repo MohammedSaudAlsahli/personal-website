491eb5a65e12f719084278d09f5e1664c585317d
Mohammed Alsahli <102217045+MohammedSaudAlsahli@users.noreply.github.com>
2025-09-04 00:36:36 +0300

    moved locla to loclas folder

--- src/utils/handleLanguageToggle.ts
+++ src/utils/handleLanguageToggle.ts
@@ -1,4 +1,12 @@
-export const handleToggle = ({ dir, i18n, toggleDirection }) => {
+import type { i18n as I18nType } from "i18next";
+
+interface ToggleProps {
+  dir: "ltr" | "rtl";
+  i18n: I18nType;
+  toggleDirection: () => void;
+}
+
+export const handleToggle = ({ dir, i18n, toggleDirection }: ToggleProps) => {
   if (dir === "rtl") {
     i18n.changeLanguage("en");
   } else {
