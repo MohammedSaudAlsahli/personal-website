4ca4de8a705ecdbf588c61e5b9b0bcf74a557fdb
Mohammed Alsahli <mohammedalsahli@Mohammeds-MacBook-Air.local>
2025-09-04 15:49:01 +0300

    obfuscator plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],base:"/",
+  plugins: [
+    react(), 
+    obfuscatorPlugin({
+      global: true,
+      rotateStringArray: true,
+      stringArray: true,
+      controlFlowFlattening: true,
+      deadCodeInjection: true
+    })],base:"/",
 })
